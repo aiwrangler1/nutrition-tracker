8c421799f5aa54898d94e38238350b714a511274
aiwranger1 <andrewrace.ny@gmail.com>
2025-01-24 23:59:48 -0500

    Clean up project structure: Remove redundant directories and files

--- app/models/Meal.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const mealSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    calories: { type: Number, required: true },
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // Reference to the user who created the meal
-});
-
-module.exports = mongoose.model('Meal', mealSchema); 
